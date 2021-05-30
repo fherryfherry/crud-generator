69a1c87166d5d60237bb909c1b0683075f3be0a0
Ferry Ariawan <ferry@crocodic.com>
2021-05-30 18:29:40 +0700

    Fixing

--- src/Assets/js/main.js
+++ src/Assets/js/main.js
@@ -21,3 +21,18 @@ function showSubMenu(t)
         $(t).data("toggle", 0);
     }
 }
+
+$(function() {
+    $('.editor').summernote({
+        height: 150,
+        toolbar: [
+            // [groupName, [list of button]]
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+        ]
+    });
+})
